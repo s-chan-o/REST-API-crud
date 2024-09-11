12c7825b442ee1d7ea0f160ccc51e1267cd86ce3
김승찬 <s24041@gsm.hs.kr>
2024-09-11 19:57:37 +0900

    fix: add context

--- src/main/java/com/example/board_crud/crud/controller/BoardController.java
+++ src/main/java/com/example/board_crud/crud/controller/BoardController.java
@@ -11,23 +11,24 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping
 public class BoardController {
 
     private final BoardService boardService;
 
     //글 등록
-    @PostMapping("/boards")
+    @PostMapping
     public BoardResponseDto createBoard(@RequestBody BoardRequestDto requestDto){
         BoardResponseDto board = boardService.createBoard(requestDto);
         return board;
     }
     //전체 목록 조회
-    @GetMapping("/boards")
+    @GetMapping
     public List<BoardListResponseDto> getAllBoards(){
         return boardService.findAllBoard();
     }
     //글 하나 조회
-    @GetMapping("/boards/{id}")
+    @GetMapping("{id}")
     public BoardResponseDto getOneBoard(@PathVariable Long id){
         return boardService.findOneBoard(id);
     }
@@ -37,12 +38,12 @@ public class BoardController {
         return boardService.updateBoard(id, requestDto);
     }
     //글 삭제
-    @DeleteMapping("/boards/{id}")
+    @DeleteMapping("{id}")
     public Long deleteBoard(@PathVariable Long id){
         return boardService.deleteBoard(id);
     }
     //비밀번호 확인
-    @GetMapping("boards/check/{id}/{inputPassword}")
+    @GetMapping("/check/{id}/{inputPassword}")
     public boolean checkPassword(@PathVariable Long id,@PathVariable String inputPassword){
         return boardService.checkPassword(id, inputPassword);
     }
